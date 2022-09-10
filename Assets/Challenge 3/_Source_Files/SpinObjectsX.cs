51cf8fdbc85c3c594b49b8d7525c6b191836a54f
Famous <52849344+Success0452@users.noreply.github.com>
2022-09-10 16:59:05 +0100

    initial commit

---
+++ Assets/Challenge 3/_Source_Files/SpinObjectsX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinObjectsX : MonoBehaviour
+{
+    public float spinSpeed;
+    public PlayerControllerX playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerControllerScript.gameOver != true)
+        {
+            transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+        }
+    }
+}
